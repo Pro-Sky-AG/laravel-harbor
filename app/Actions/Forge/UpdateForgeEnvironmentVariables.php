6f8717d00a65c7362f15ef067d53482bed4e5b33
Mehran Rasulian <7046255+mehrancodes@users.noreply.github.com>
2025-11-05 20:41:20 +0400

    Revert "Temporary fix for updating environment variables,"

--- app/Actions/Forge/UpdateForgeEnvironmentVariables.php
+++ app/Actions/Forge/UpdateForgeEnvironmentVariables.php
@@ -8,7 +8,6 @@ use App\Actions\MergeEnvironmentVariables;
 use App\Actions\TextToArray;
 use App\Services\Forge\ForgeService;
 use App\Traits\Outputifier;
-use Illuminate\Support\Facades\Http;
 
 class UpdateForgeEnvironmentVariables
 {
@@ -38,35 +37,11 @@ class UpdateForgeEnvironmentVariables
         $source = $service->forge->siteEnvironmentFile($service->server->id, $service->site->id);
         $mergedEnvs = MergeEnvironmentVariables::run($source, $newKeys);
 
-        // TODO: This is a temporary solution. Direct HTTP call to Forge API should be replaced
-        // with proper SDK support once available. Remove this conditional when SDK supports
-        // organization-based endpoints.
-        $useNewApi = env('USE_NEW_FORGE_API', false) === true || env('USE_NEW_FORGE_API') === 'true';
-        $organization = env('FORGE_ORGANIZATION');
-
-        if ($useNewApi && $organization) {
-            $url = sprintf(
-                'https://forge.laravel.com/api/orgs/%s/servers/%s/sites/%s/environment',
-                $organization,
-                $service->server->id,
-                $service->site->id
-            );
-
-            Http::withHeaders([
-                'Authorization' => sprintf('Bearer %s', $service->setting->token),
-                'Content-Type' => 'application/json',
-            ])->put($url, [
-                'environment' => $mergedEnvs,
-                'cache' => true,
-                'queues' => true,
-            ]);
-        } else {
-            $service->forge->updateSiteEnvironmentFile(
-                $service->server->id,
-                $service->site->id,
-                $mergedEnvs
-            );
-        }
+        $service->forge->updateSiteEnvironmentFile(
+            $service->server->id,
+            $service->site->id,
+            $mergedEnvs
+        );
 
         return true;
     }
